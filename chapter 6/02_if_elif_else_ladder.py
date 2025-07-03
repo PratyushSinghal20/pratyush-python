d314baf046290d128a25e75f3d97a8f366f823c2
Pratyush Singhal <pratyush1820@gmail.com>
2025-07-03 10:57:32 +0530

    Add files via upload

---
+++ chapter 6/02_if_elif_else_ladder.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+
+if (a>18):
+    print("You can vote")
+elif (a==18):
+    print("You can vote but you need to register first")    
+else:
+    print("You cannot vote")
---
+++ chapter 6/problem5.py
@@ -0,0 +1,8 @@
+l = ["harry" , "sachin", "pratyush", "tanish", "sahil", "rohan", "shubham" ]
+
+name = input("enter your name:")
+
+if (name in l):
+    print("Welcome to the party")
+else:
+    print("You are not invited to the party")
