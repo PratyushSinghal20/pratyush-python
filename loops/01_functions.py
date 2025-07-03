ca6e3c9e3d523eb22791c240812dbb826766637f
Pratyush Singhal <pratyush1820@gmail.com>
2025-07-03 19:47:10 +0530

    Create 01_functions.py

---
+++ loops/01_functions.py
@@ -0,0 +1,15 @@
+a = 12
+b = 45
+c = 56
+
+average = (a+b+c)/3
+print (average) 
+
+def avg():
+ 
+  a = (int(input("Enter the first number: ")))
+  b = (int(input("Enter the second number: "))) 
+  c = (int(input("Enter the third number: ")))
+
+average = (a+b+c)/3
+print("The average of the three numbers is: ", average)
