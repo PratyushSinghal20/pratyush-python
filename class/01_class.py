ac567d6415b58266e13184fdc61a73302f1e92be
Pratyush Singhal <pratyush1820@gmail.com>
2025-07-18 13:59:40 +0530

    Create 01_class.py

---
+++ class/01_class.py
@@ -0,0 +1,12 @@
+class Employee: 
+    language = "Py" 
+    salary = 1200000 
+
+
+harry = Employee()
+harry.name = "Harry" 
+print(harry.name, harry.language, harry.salary)
+
+rohan = Employee()
+rohan.name = "Rohan Roro Robinson"
+print(rohan.name, rohan.salary, rohan.language)
