14afd6f7e5f1546fedf35f67a1d5cd585cdb1566
PratyushSinghal20 <pratyush1820@gmail.com>
2025-08-24 17:16:12 +0530

    Initial Commit

---
+++ practice/9.py
@@ -0,0 +1,31 @@
+# Integer
+a = 10
+print("Integer:", a, type(a))
+
+# Float
+b = 10.5
+print("Float:", b, type(b))
+
+# String
+c = "Python"
+print("String:", c, type(c))
+
+# Boolean
+d = True
+print("Boolean:", d, type(d))
+
+# List
+e = [1, 2, 3, 4]
+print("List:", e, type(e))
+
+# Tuple
+f = (1, 2, 3, 4)
+print("Tuple:", f, type(f))
+
+# Dictionary
+g = {"name": "Alice", "age": 20}
+print("Dictionary:", g, type(g))
+
+# Set
+h = {1, 2, 3, 4}
+print("Set:", h, type(h))
