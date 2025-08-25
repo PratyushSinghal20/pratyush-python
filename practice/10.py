66447bf1c1fe2c05a86d796248771eedc4060377
PratyushSinghal20 <pratyush1820@gmail.com>
2025-08-25 17:03:14 +0530

    Initial Commit

---
+++ practice/10.py
@@ -0,0 +1,38 @@
+a, b = 15, 4
+
+# Arithmetic
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+print("Floor Division:", a // b)
+
+# Relational
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
+print("a != b:", a != b)
+
+
+# Assignment
+c = 10
+c += 5
+print("Assignment (c += 5):", c)
+
+# Logical
+x, y = True, False
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
+
+
+
+# Bitwise
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT (~a):", ~a)
+print("Bitwise Shift Left:", a << 1)
+print("Bitwise Shift Right:", a >> 1)
---
+++ practice/11.py
@@ -0,0 +1,5 @@
+import math
+
+radius = float(input("Enter radius of the circle: "))
+area = math.pi * radius * radius
+print("Area of Circle =", area)
