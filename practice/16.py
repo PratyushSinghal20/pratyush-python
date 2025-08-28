c2d7d79046e5152e104c92d9b9625fda8c595e92
PratyushSinghal20 <pratyush1820@gmail.com>
2025-08-28 23:04:16 +0530

    Initial Commit

---
+++ practice/16.py
@@ -0,0 +1,17 @@
+# Demonstrating for loop with range()
+
+print("Numbers from 0 to 4:")
+for i in range(5):
+    print(i)
+
+print("\nNumbers from 2 to 6:")
+for i in range(2, 7):
+    print(i)
+
+print("\nEven numbers from 2 to 10:")
+for i in range(2, 11, 2):
+    print(i)
+
+print("\nNumbers in reverse from 10 to 1:")
+for i in range(10, 0, -1):
+    print(i)
